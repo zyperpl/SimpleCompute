418e6e8ddc0060b32437c7e6ea5af19b0cb86ed2
Kacper Zybała <zyperpl@gmail.com>
2025-08-28 23:47:49 +0200

    Add export macro

--- compute.h
+++ compute.h
@@ -3,6 +3,22 @@
 
 #include <stdbool.h>
 
+#ifdef _WIN32
+  #ifdef SCOMPUTE_BUILD_DLL
+    #define SCEXPORT __declspec(dllexport)
+  #elif defined(SCOMPUTE_USE_DLL)
+    #define SCEXPORT __declspec(dllimport)
+  #else
+    #define SCEXPORT
+  #endif
+#else
+  #ifdef SCOMPUTE_BUILD_SHARED
+    #define SCEXPORT __attribute__((visibility("default")))
+  #else
+    #define SCEXPORT
+  #endif
+#endif
+
 typedef struct ComputeShader
 {
   int id;
@@ -21,20 +37,20 @@ extern "C"
 {
 #endif
 
-ComputeShader load_compute_shader(const char *source);
-ComputeBuffer create_compute_buffer(void *data, int count);
+SCEXPORT ComputeShader load_compute_shader(const char *source);
+SCEXPORT ComputeBuffer create_compute_buffer(void *data, int count);
 
-bool is_compute_shader_valid(ComputeShader shader);
-bool is_compute_buffer_valid(ComputeBuffer buffer);
+SCEXPORT bool is_compute_shader_valid(ComputeShader shader);
+SCEXPORT bool is_compute_buffer_valid(ComputeBuffer buffer);
 
-void compute_dispatch(ComputeShader *shader, ComputeBuffer buffer);
-bool is_compute_done(ComputeShader shader);
+SCEXPORT void compute_dispatch(ComputeShader *shader, ComputeBuffer buffer);
+SCEXPORT bool is_compute_done(ComputeShader shader);
 
-void copy_compute_buffer_to_texture(ComputeBuffer buffer, unsigned int texture_id, int width, int height);
-void set_shader_uniform_float(ComputeShader shader, const char *name, float *value);
+SCEXPORT void copy_compute_buffer_to_texture(ComputeBuffer buffer, unsigned int texture_id, int width, int height);
+SCEXPORT void set_shader_uniform_float(ComputeShader shader, const char *name, float *value);
 
-void unload_compute_shader(ComputeShader shader);
-void unload_compute_buffer(ComputeBuffer buffer);
+SCEXPORT void unload_compute_shader(ComputeShader shader);
+SCEXPORT void unload_compute_buffer(ComputeBuffer buffer);
 
 #if defined(__cplusplus)
 }
